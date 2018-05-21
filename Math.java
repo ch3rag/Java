0d3115269f78147e6c7c3229aa24bbeb2e5b296b
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-22 02:05:17 +0530

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,30 @@
+public class MyClass {
+
+	public static void main(String args[]) {
+	
+		// Absolute Value
+
+		System.out.println("Absolute of -5.23 : " + Math.abs(-5.23));
+
+		// Ceiling 
+
+		System.out.println("Ceiling of 5.23 : " + Math.ceil(5.23));
+
+		// Floor
+
+		System.out.println("Floor of 5.23 : " + Math.floor(5.23));
+
+		// Round 
+
+		System.out.println("Rounding 5.23 : " + Math.round(5.23));
+
+		// Square Root
+
+		System.out.println("Square root of 225 : " + Math.sqrt(225));
+
+		// Random Numbers
+
+		System.out.println("Random Number : " + (int) (Math.random() * 10)); //Math.random() return 0 - 0.9999999
+
+	}
+}
