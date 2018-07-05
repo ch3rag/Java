496c6e11244da0a389cdd4480158b43909cb5e2d
Bharat Singh Rajput <bharatsr1997@gmail.com>
2018-07-06 00:51:33 +0530

    Update AbstractTestDrive.java

--- Head First Java/Chapter - 8/AbstractTestDrive.java
+++ Head First Java/Chapter - 8/AbstractTestDrive.java
@@ -25,6 +25,10 @@ class Dog extends Animal {
 		System.out.println("Woof!");
 	}
 
+	public void sleep() {
+		System.out.println(getName() + " is Sleeping!");
+	}
+
 }
 
 class Cat extends Animal {
@@ -49,7 +53,7 @@ class Vet {
 public class AbstractTestDrive {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList <Animal> myAnimals = new ArrayList <Animal> ();
 		//Animal myAnimal = new Animal();  
 		//This is illegal since abstract class can't be instantiated
@@ -57,14 +61,35 @@ public class AbstractTestDrive {
 		Animal myDog = new Dog();
 		Animal myCat = new Cat();
 
+		Dog myDog2 = new Dog();
+
+		
 		myDog.setName("Tommy");
 		myCat.setName("Nancy");
+		
+		//Illegal!
+		//myDog.sleep();
+		//Since reference is Animal type, the methods defined in Animals call are allowed to be called
 
+		myDog2.sleep();
+		//Legal, since reference type is Dog
+		
 		myAnimals.add(myDog);
 		myAnimals.add(myCat);
 
 		for(Animal animal : myAnimals) {
 			myVet.treatAnimal(animal);
 		}
+
+		//The Classes which doesn't explicitly extend any onther class inherits "Object" class
+
+		//Some of its method include
+
+		System.out.println(myDog.getClass());
+		System.out.println(myDog.hashCode());
+		System.out.println(myDog.toString());
+		System.out.println(myDog.equals(myCat));
+		System.out.println(myDog.equals(myDog2));
+
 	}
 }
