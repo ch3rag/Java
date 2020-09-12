da07db267750c9da12423880f15de78189cd6e56
Bharat Singh Rajput <bharatsr1997@gmail.com>
2020-09-12 17:22:50 +0530

    Update

---
+++ HowToJava/1. Introduction/HelloWorld2.java
@@ -0,0 +1,7 @@
+public class HelloWorld2 {
+	public static void main(String[] args) {
+		System.out.print("Hello, ");
+		System.out.println("World!");
+		System.out.println("Welcome\nTo\nJava\nProgramming!");
+	}
+}
---
+++ HowToJava/1. Introduction/PrintfExample.java
@@ -0,0 +1,7 @@
+public class PrintfExample {
+	public static void main(String[] args) {
+		System.out.printf("%s\n%s\n", "Welcome To", "Java Programming"); 
+		// %n is portable accross platforms
+		System.out.printf("%s%n%s%n", "Welcome To", "Java Programming");
+	}
+}
